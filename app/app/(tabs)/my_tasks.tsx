3dbb74e803b44cbbd7ae060b3a42a890e4f2eca9
Kevin Torres <kevintu236@gmail.com>
2025-09-13 18:34:32 -0600

    Filtro de búsqueda implementado
    - Utilizando la librería "loadsh.filter", ahora es posible buscar las tareas y filtrarlas por nombre o descripción.

--- app/app/(tabs)/my_tasks.tsx
+++ app/app/(tabs)/my_tasks.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
-import { Platform, StyleSheet, SafeAreaView, View, ScrollView, Text, TouchableOpacity } from 'react-native';
+import { Platform, StyleSheet, SafeAreaView, View, ScrollView, Text, TouchableOpacity, TextInput, FlatList } from 'react-native';
 import axios from 'axios';
 import Task from '@/components/Task';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import EditableTask from '@/components/EditableTask';
 import { useCompletedTasks } from '@/store/storeCompletedTasks';
+import filter from "lodash.filter";
 
 type People = {
   name: string,
@@ -26,15 +27,25 @@ type TaskBody = {
   _id: string,
 }
 
+type Tasks = {
+  tags: [Tags],
+  title: string,
+  description: string,
+  collaborators: [People],
+}
+
 export default function TabTwoScreen() {
 
-  const [tasks, setTasks] = useState<TaskBody[]>([]);
+  const [tasks, setTasks] = useState<any[]>([]);
+  const [filteredTasks, setFilteredTasks] = useState<any[]>([]);
   const [removeModal, setRemoveModal] = useState(false);
   const [editModal, setEditModal] = useState(false);
   const [deletionId, setDeletionId] = useState("");
   const [editionId, setEditionId] = useState("");
   const addTask = useCompletedTasks(state => state.addTask);
   const [completionMsg, setCompletionMsg] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [fullData, setFullData] = useState([]);
 
   function onRemoveTask(id: string) {
     setRemoveModal(!removeModal);
@@ -93,6 +104,7 @@ export default function TabTwoScreen() {
       try {
         const response = await axios.get("http://192.168.1.65:8080/api/task/taskGet");
         setTasks(response.data);
+        setFullData(response.data);
 
       } catch (error) {
         console.log("Error while fetching the data", error);
@@ -106,7 +118,28 @@ export default function TabTwoScreen() {
     setTimeout(function () {
       setCompletionMsg(false);
     }, 3000);
-  }, [completionMsg])
+  }, [completionMsg]);
+
+  function handleSearch(query: string) {
+    console.log(!searchQuery);
+    setSearchQuery(query);
+    const formattedQuery = query.toLowerCase();
+    console.log(formattedQuery);
+    const filteredData = filter(fullData, (task) => {
+      return contains(task, formattedQuery);
+    });
+    console.log(filteredData);
+    setFilteredTasks(filteredData);
+  };
+
+  const contains = ({ tags, title, description, collaborators }: Tasks, query: string) => {
+    const formattedTitle = title.toLowerCase();
+    const formattedDesc = description.toLowerCase();
+    if (formattedTitle.includes(query) || formattedDesc.includes(query)) {
+      return true;
+    }
+    return false;
+  };
 
   return (
     <SafeAreaView style={styles.safe}>
@@ -138,6 +171,16 @@ export default function TabTwoScreen() {
         <View className='items-center justify-center h-full w-full gap-6 px-6'>
           <Text className='self-start text-[40px] font-bold tracking-[-0.1rem] text-slate-800'>My Tasks</Text>
 
+          <View className='w-full pl-6 bg-slate-100 border border-slate-200 rounded-xl flex-row items-center justify-center'>
+            <Ionicons name='search' size={18} color="#64748b" />
+            <TextInput
+              defaultValue={searchQuery}
+              clearButtonMode='always'
+              onChangeText={query => handleSearch(query)}
+              placeholder='Search a task'
+              className='w-full placeholder:text-lg' />
+          </View>
+
           <View className='gap-4 pb-14 w-full'>
 
             <View className='pending-section w-full bg-[rgba(251,147,1,0.15)] bg-op py-4 px-4 rounded-xl justify-between flex-row items-center'>
@@ -151,9 +194,21 @@ export default function TabTwoScreen() {
               </TouchableOpacity>
             </View>
 
-            {tasks?.map((task, index) => (
-              <Task key={index} isCompleted={false} tags={task.tags} title={task.title} desc={task.description} collaborators={task.collaborators} onRemove={() => onRemoveTask(task._id)} onComplete={() => onCompleteTask(task._id, index)} onEdit={() => onEditTask(task._id)} />
-            ))}
+            {!searchQuery && (
+              <View className='gap-3'>
+                {tasks?.map((task, index) => (
+                  <Task key={index} isCompleted={false} tags={task.tags} title={task.title} desc={task.description} collaborators={task.collaborators} onRemove={() => onRemoveTask(task._id)} onComplete={() => onCompleteTask(task._id, index)} onEdit={() => onEditTask(task._id)} />
+                ))}
+              </View>
+            )}
+
+            {searchQuery && (
+              <View className='gap-3'>
+                {filteredTasks?.map((task, index) => (
+                  <Task key={index} isCompleted={false} tags={task.tags} title={task.title} desc={task.description} collaborators={task.collaborators} onRemove={() => onRemoveTask(task._id)} onComplete={() => onCompleteTask(task._id, index)} onEdit={() => onEditTask(task._id)} />
+                ))}
+              </View>
+            )}
 
             {completionMsg && (
               <View className='bg-green-100 py-3 w-full self-center items-center justify-center flex-row gap-2'>
